bfbc5b4e056ae66c03abc5295385760c7c37337c
caiorodriveria <rodrigocaio2775@gmail.com>
2023-07-20 13:08:12 -0300

    FISRT - Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qr-code-scanner';
+
+  @ViewChild('videoElement') video!: ElementRef<HTMLVideoElement>
+
+  constructor(){
+
+  }
+
+  //após a página ser completamente carregada
+  ngAfterViewInit(): void {
+    this.prepareScanner();
+  }
+
+  async prepareScanner(){
+    const available = await this.checkCamera();
+    if(available) this.startScanner();
+  }
+
+  async startScanner() {
+    
+  }
+
+  //verifica dse possui permissão do navegador de camêra
+  async checkCamera(){
+    const cameraPermission = await navigator.permissions.query({name: 'camera'} as any)
+    console.log(cameraPermission);
+    const isOk = cameraPermission.state !== "denied";
+
+    const hasMediaDevice = 'mediaDevices' in navigator;
+    const hasUserMedia = 'getUserMedia' in navigator.mediaDevices
+
+    if(!hasMediaDevice || (!hasUserMedia && isOk)){
+      alert("Nao conseguimos acesso a camera, por favor verifique");
+    }
+
+    return isOk;
+  }
 }
